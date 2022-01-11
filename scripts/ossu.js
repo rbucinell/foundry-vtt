0516527aedbbf027b0612ae757a5f8d35c3cda5f
Bucinell, Ryan <rbucinell.dev@gmail.com>
2022-01-11 14:17:36 -0500

    fixed data-roll object

--- scripts/ossu.js
+++ scripts/ossu.js
@@ -1,8 +1,6 @@
 let renderInlineRoll = function( roll )
 {
 	let rollJSONstr = encodeURI(JSON.stringify(roll.toJSON()));
-	console.log( rollJSONstr);
-	rollJSONstr='';
 	const faDice = `fa-dice${attackRoll.terms.find( t => t instanceof Die ).faces === 20?'-20':''}`;
 	
 	return 
@@ -17,7 +15,7 @@ let renderInlineRoll = function( roll )
 	target: Actor5e
 	weapon: Item53 (of type='weapon')
 **/
-let makeAttak = async function( attacker, weapon )
+let makeAttack = async function( attacker, weapon )
 {
 	if( attacker === undefined)
 	{
@@ -28,13 +26,15 @@ let makeAttak = async function( attacker, weapon )
 	let dex = attacker.data.data.abilities.dex;
 	
 	//Make an attack roll
-	let attackRoll = new Roll("1d20kh + @prof + @dexMod + @weaponBonus", { 
+    let attackFormula = "1d20kh + @prof + @dexMod + @weaponBonus";
+	let attackRoll = new Roll(attackFormula, { 
 		prof: attacker.data.data.prof.flat, 
 		dexMod: dex.mod,
 		weaponBonus: weapon.data.data.attackBonus
 	});
+    attackRoll.creationFormula = attackFormula;
 	
-	await attackRoll.evaluate(async=true);
+	await attackRoll.evaluate({async:true});
 	let isNat20 = attackRoll.terms[0].results[0].result === 20;
 	let isNat1  = attackRoll.terms[0].results[0].result === 1;	
 	let critStr = '';
@@ -46,9 +46,14 @@ let makeAttak = async function( attacker, weapon )
 			critStr = '+ 2d6'
 		}
 	}
-	let damageRoll = new Roll(`1d6 + ${dex.mod} ${critStr}`);
+    let damageFormula = "1d6 + @dexMod + @critBonus";
+	let damageRoll = new Roll(damageFormula,{
+        dexMod: dex.mod,
+        critBonus: critStr
+    });
+    damageRoll.creationFormula = damageFormula;
 	
-	await damageRoll.evaluate(async=true);
+	await damageRoll.evaluate({async:true});
 	
 	return {
 		attack: attackRoll,
@@ -60,18 +65,27 @@ let makeAttak = async function( attacker, weapon )
 
 let ossu = game.user.character;
 
-let viciousScimitar = ossu.data.items.find( item => item.data.name === 'Vicious Scimitar' );
-let regularScimitar = ossu.data.items.find( item => item.data.name === 'Scimitar' );
-
-let mainhand = await makeAttak( ossu, viciousScimitar );
-let offhand  = await makeAttak( ossu, regularScimitar );
+let weapon = null, weapon2 = null;
+if( location.href === 'https://demo.foundryvtt.com/game' )
+{
+    weapon = ossu.data.items.find( item => item.data.type === 'weapon');
+    weapon2 = ossu.data.items.find( item => item.data.type === 'weapon');
+}
+else
+{
+    weapon  = ossu.data.items.find( item => item.data.name === 'Vicious Scimitar' );
+    weapon2 = ossu.data.items.find( item => item.data.name === 'Scimitar' );
+}
+let mainhand = await makeAttack( ossu, weapon );
+let offhand  = await makeAttack( ossu, weapon2 );
 
 ChatMessage.create({ content:
     `Mainhand attack 
-    <a class="inline-roll inline-result" title="1d20"><i class="fas fa-dice-d20"></i>${mainhand.attack.total}</a> dealing 
-    <a class="inline-roll inline-result" title="1d6"><i class="fas fa-dice-six"></i>${mainhand.damage.total}</a> damage. 
+    <a class="inline-roll inline-result" data-roll=${encodeURI(JSON.stringify(mainhand.attack.toJSON()))} title="${mainhand.attack.formula}"><i class="fas fa-dice-d20"></i>${mainhand.attack.total}</a> dealing 
+    <a class="inline-roll inline-result" data-roll=${encodeURI(JSON.stringify(mainhand.damage.toJSON()))} title="${mainhand.attack.formula}"><i class="fas fa-dice"></i>${mainhand.damage.total}</a> damage. 
     <br> 
     Offhand attack 
-    <a class="inline-roll inline-result" title="1d20"><i class="fas fa-dice-d20"></i>${offhand.attack.total}</a> dealing 
-    <a class="inline-roll inline-result" title="1d6"><i class="fas fa-dice-six"></i>${offhand.damage.total}</a> damage`});
+    <a class="inline-roll inline-result" data-roll=${encodeURI(JSON.stringify(offhand.attack.toJSON()))} title="${offhand.attack.formula}"><i class="fas fa-dice-d20"></i>${offhand.attack.total}</a> dealing 
+    <a class="inline-roll inline-result" data-roll=${encodeURI(JSON.stringify(offhand.damage.toJSON()))} title="${offhand.attack.formula}"><i class="fas fa-dice"></i>${offhand.damage.total}</a> damage`
+});
 //ChatMessage.create({ content:`Mainhand attack ${renderInlineRoll(mainhand.attack)} dealing ${renderInlineRoll(mainhand.damage)} damage. Offhand attack <a class="inline-roll inline-result" title="1d20"><i class="fas fa-dice-d20"></i>${offhand.attack.total}</a> dealing <a class="inline-roll inline-result" title="1d6"><i class="fas fa-dice-six"></i>${offhand.damage.total}</a> damage`});
